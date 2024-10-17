1dd1b4e8ae81265708c3016247dfaaa88b2e2b9f
Miguel Abril <miguel.abril.alamo.al@iespoligonosu.org>
2024-10-17 16:44:51 +0200

    anyado los sitios a admin

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site(Usuario)
+admin.site(Categoria)
+admin.site(Producto)
+admin.site(ProductoCategoria)
+admin.site(Calzado)
+admin.site(Muebles)
+admin.site(Chat)
+admin.site(Compra)
+admin.site(CompraProducto)
+admin.site(Envio)
+admin.site(Valoracion)
