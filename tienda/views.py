bda06b45007e36421be2c22f2da85e72a2739a21
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-23 11:08:08 +0200

    listar y mostrar uno

--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q,F,Prefetch
+from django.db.models import Avg,Max,Min
+from .models import Producto, Usuario, Categoria, ProductoCategoria
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+#Esta view muestra una lista con toda la informacion de todos los productos que hay
+def listar_productos (request):
+    producto = Producto.objects.select_related("vendedor").prefetch_related("categorias")
+    producto = producto.all()
+    return render(request, "productos/lista.html", {"producto_mostrar":producto})
+
+def muestra_producto(request, id_producto):
+    QSproducto = Producto.objects.select_related("vendedor").prefetch_related("categorias")
+    producto = QSproducto.get(id=id_producto)
+    return render(request, "productos/producto.html", {"producto_mostrar":producto})
