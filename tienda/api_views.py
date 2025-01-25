942749800bb5f1d1227a07c191d0ea4b2abc184a
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-01-25 18:40:00 +0100

    empiezo consulta mejorada

--- tienda/api_views.py
+++ tienda/api_views.py
@@ -9,3 +9,10 @@ def producto_listar(request):
     productos = Producto.objects.select_related('vendedor').prefetch_related('categorias').all()
     serializer = ProductoSerializer(productos,many = True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def producto_listar_avanzado(request):
+    productos = Producto.objects.select_related('vendedor').prefetch_related('categorias',
+                                                                             Prefetch('producto_compra')).all()
+    serializer = ProductoSerializer(productos,many = True)
+    return Response(serializer.data)
