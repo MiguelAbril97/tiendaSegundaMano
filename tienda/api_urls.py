69cc0edf2417553d79bab717730bc5e3619bb6d5
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-02-04 09:23:08 +0100

    creo las url de las view producto buscar

--- tienda/api_urls.py
+++ tienda/api_urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('productos-mejorado/',producto_listar_mejorado),
     path('calzados/',calzado_listar),
     path('consolas/',consolas_listar),
+    path('productos-buscar-simple/',producto_buscar_simple),
+    path('productos-buscar/',producto_buscar),
 ]
